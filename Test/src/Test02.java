910f41ac89462ef25e3ac3969db76107efe8ca85
Sasada Yusuke <sasada.yusuke@eiwa.com>
2015-11-16 00:31:54 +0900

    Test

---
+++ Test/src/Test02.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2015 EIS Co., Ltd. All rights reserved.
+ */
+
+/**
+ * @author （作成者） <br />
+ *  （クラス論理名） <br />
+ *  （説明） <br />
+ *  更新履歴 yyyy/mm/dd （更新者）：（説明） <br />
+ */
+public class Test02 {
+
+  /**
+   * （メソッド論理名） <br />
+   * （説明） <br />
+   * @param args
+   */
+  public static void main( String[] args ) {
+    // TODO 自動生成されたメソッド・スタブ
+    System.out.println( "ようこそJava勉強会へ。\n"
+    + "これは2つ目のJavaプログラムです。" );
+  }
+
+}
+
---
+++ Test/src/Test03.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2015 EIS Co., Ltd. All rights reserved.
+ */
+
+/**
+ * @author （作成者） <br />
+ *  （クラス論理名） <br />
+ *  （説明） <br />
+ *  更新履歴 yyyy/mm/dd （更新者）：（説明） <br />
+ */
+public class Test03 {
+
+  /**
+   * （メソッド論理名） <br />
+   * （説明） <br />
+   * @param args
+   */
+  public static void main( String[] args ) {
+    // TODO 自動生成されたメソッド・スタブ
+    //args new String[2]
+    //args[0] = "こんにちは"
+    //args[1] = "おはよう"
+    System.out.println( args[1] );
+  }
+}
